adf960a1a8805797ac4b18cc59047c5d5b5ea8d8
abu-slipstream <abi.fly999@gmail.com>
2024-07-19 14:50:48 +0400

    commit4

--- src/app/components/insuranceQuote/InsuranceQuote.jsx
+++ src/app/components/insuranceQuote/InsuranceQuote.jsx
@@ -70,46 +70,7 @@ function InsuranceQuote() {
               {/* Row3 */}
               <div className="rounded-lg sm:col-span-6 sm:block">
                 <p>Is you car brand new ?</p>
-                <Field name="brand1"/>
-                <ul class="">
-                  <div className=" grid gap-4 sm:grid-cols-12">
-                    <div className=" rounded-lg sm:col-span-6 sm:block hidden">
-                      <li>
-                        <input
-                          type="radio"
-                          id="option-yes"
-                          name="hosting"
-                          value="option-yes"
-                          class="hidden peer"
-                          required
-                        />
-                        <label
-                          for="option-yes"
-                          class="inline-flex items-center text-center w-full p-3 text-gray-500  rounded-lg cursor-pointer peer-checked:border-black border-2 dark:border-gray-200 dark:peer-checked:text-black  dark:hover:bg-gray-100"
-                        >
-                          <div class="w-full text-md text-center">Yes</div>
-                        </label>
-                      </li>
-                    </div>
-                    <div className="rounded-lg sm:col-span-6 sm:block hidden">
-                      <li>
-                        <input
-                          type="radio"
-                          id="option-no"
-                          name="hosting"
-                          value="option-no"
-                          class="hidden peer"
-                        />
-                        <label
-                          for="option-no"
-                          class="inline-flex items-center text-center w-full p-3 text-gray-500  rounded-lg cursor-pointer peer-checked:border-black border-2 dark:border-gray-200 dark:peer-checked:text-black  dark:hover:bg-gray-100"
-                        >
-                          <div class="w-full text-md text-center">No</div>
-                        </label>
-                      </li>
-                    </div>
-                  </div>
-                </ul>
+                <Field name="brand1" placeholder="Yes / NO"/>
                 <ErrorMessage name="brand1" />
               </div>
               <div className="rounded-lg sm:col-span-6 sm:block">

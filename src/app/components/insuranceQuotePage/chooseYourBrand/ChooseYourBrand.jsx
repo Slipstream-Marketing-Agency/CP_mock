f1d37f9dd8ff414300628d0ba0e69abbb19755b7
abu-slipstream <abi.fly999@gmail.com>
2024-08-01 12:21:14 +0400

    minor style updates

--- src/app/components/insuranceQuotePage/chooseYourBrand/ChooseYourBrand.jsx
+++ src/app/components/insuranceQuotePage/chooseYourBrand/ChooseYourBrand.jsx
@@ -1,14 +1,7 @@
 import Image from "next/image";
-import benzIcon from "./../../../../../public/benz-icon.svg";
-import mistubishiIcon from "./../../../../../public/mistubishi-icon.svg";
-import nissanIcon from "./../../../../../public/nissan-icon.svg";
-import fiatIcon from "./../../../../../public/fiat-icon.svg";
-import brandIcon from "./../../../../../public/brand-icon.png";
 import { carData } from "../../../mocks/mock";
 
 export default function ChooseYourBrand() {
-    const image = [benzIcon, mistubishiIcon, nissanIcon, benzIcon, fiatIcon, brandIcon, benzIcon, mistubishiIcon, nissanIcon, benzIcon, fiatIcon, brandIcon,]
-
     return (
         <div>
             <div className="heading-blue">CHOOSE YOUR BRAND</div>
@@ -17,7 +10,7 @@ export default function ChooseYourBrand() {
             <ul className='my-6 grid gap-2 grid-cols-12'>
                 {carData.chooseBrandModal.map(brand => (
                     <li className='col-span-3 sm:col-span-2 my-6'>
-                        <Image src={brand.image} width={64} height={64} className=" opacity-50" alt="brand-icon" />
+                        <Image src={brand.image} width={64} height={64} className=" opacity-50 hover:opacity-100 cursor-pointer" alt="brand-icon" />
                     </li>
                 ))}
             </ul>
